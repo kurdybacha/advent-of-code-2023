b793436548ed747ca961b0a60f2caf6ae0ad1af1
Pawel Kurdybacha <kurdybacha@users.noreply.github.com>
2023-12-04 22:14:16 +0000

    Use C++ ranges for simpler Day 4 code

--- day04/solve.cpp
+++ day04/solve.cpp
@@ -4,29 +4,23 @@
 #include <iterator>
 #include <numeric>
 #include <ranges>
-#include <set>
 #include <unordered_map>
+#include <unordered_set>
 
 using namespace std;
 
-set<int> parse(string_view sv) {
-    int pos = 0;
-    set<int> result;
-    string_view n;
-    for (int i = 0; i < sv.size(); ++i) {
-        if (sv[i] == ' ') {
-            if (!n.empty()) result.emplace(stoi(n.data()));
-            pos = i;
-            n = string_view{};
-            continue;
-        }
-        if (n.empty()) pos = i;
-        n = sv.substr(pos, i - pos + 1);
-    }
-    if (!n.empty()) {
+unordered_set<int> parse(string_view sv) {
+    unordered_set<int> result;
+    for (const auto &n : sv | views::split(' ')) {
         result.emplace(stoi(n.data()));
     }
     return result;
+}
+
+unordered_set<int> intersection(const unordered_set<int> &a,
+                                const unordered_set<int> &b) {
+    auto view = a | views::filter([&b](int e) { return b.contains(e); });
+    return unordered_set<int>(begin(view), end(view));
 }
 
 int main(int argc, char **argv) {
@@ -38,25 +32,20 @@ int main(int argc, char **argv) {
     while (getline(file, line)) {
         ++cardNo;
         string_view vline{line};
-        auto numbers = vline.substr(vline.find_first_of(':') + 1);
-        auto winning_numbers = numbers.substr(0, numbers.find_first_of('|'));
-        auto your_numbers = numbers.substr(numbers.find_first_of('|') + 1);
-        auto winning_set = parse(winning_numbers);
-        auto yours_set = parse(your_numbers);
-        set<int> out;
-        ranges::set_intersection(winning_set, yours_set,
-                                 inserter(out, begin(out)));
+        auto numbers = vline.substr(vline.find_first_of(':') + 2);
+        auto winning_set =
+            parse(numbers.substr(0, numbers.find_first_of('|') - 1));
+        auto my_set = parse(numbers.substr(numbers.find_first_of('|') + 2));
+        auto my_winning = intersection(my_set, winning_set);
         cards[cardNo] += 1;
-        if (!out.empty()) {
-            for (int i = cardNo + 1; i < cardNo + out.size() + 1; ++i) {
+        if (!my_winning.empty()) {
+            for (int i = cardNo + 1; i < cardNo + my_winning.size() + 1; ++i) {
                 cards[i] += cards[cardNo];
             }
-            points += 1 << (out.size() - 1);
+            points += 1 << (my_winning.size() - 1);
         }
     }
     cout << "Part 1: " << points << "\n";
     cout << "Part 2: "
-         << accumulate(begin(cards), end(cards), 0,
-                       [](int val, const auto &p) { return val + p.second; })
-         << "\n";
+         << ranges::fold_left(cards | views::values, 0, plus<int>()) << "\n";
 }
