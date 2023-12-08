87f176c253120ff98d60a7a08854ed06daaa9919
Pawel Kurdybacha <kurdybacha@users.noreply.github.com>
2023-12-08 09:52:19 +0000

    Day 8 Part 2 in C++

--- day08/solve.cpp
+++ day08/solve.cpp
@@ -14,7 +14,7 @@
 
 using namespace std;
 
-int main(int argc, char **argv) {
+int main(int argc, char** argv) {
     fstream file{argv[1]};
     string line;
 
@@ -25,21 +25,36 @@ int main(int argc, char **argv) {
 
     unordered_map<string, pair<string, string>> nodes;
 
+    vector<string> poss;
     while (getline(file, line)) {
         string node = line.substr(0, 3);
         string left = line.substr(7, 3);
         string right = line.substr(12, 3);
         nodes.emplace(node, make_pair(left, right));
+        if (node[2] == 'A') {
+            poss.push_back(node);
+        }
     }
 
-    string pos = "AAA";
-    int i = 0;
-    while (pos != "ZZZ") {
-        auto &d = nodes[pos];
-        auto &next =
-            directions[i % directions.size()] == 'L' ? d.first : d.second;
-        pos = next;
-        i++;
+    vector<ulong> counts(poss.size());
+    for (const auto& [idx, pos] : poss | views::enumerate) {
+        ulong i = 0;
+        string p = pos;
+        while (p[2] != 'Z') {
+            char direction = directions[i % directions.size()];
+            auto d = nodes[p];
+            auto next = direction == 'L' ? d.first : d.second;
+            p = next;
+            ++i;
+        }
+        counts[idx] = i;
     }
-    cout << format("Part 1: {}\n", i);
+    cout << format(
+        "Part 1: {}\n",
+        counts[distance(begin(poss), find(begin(poss), end(poss), "AAA"))]);
+
+    auto l = ranges::fold_left(counts, 1,
+                               [](ulong a, ulong b) { return lcm(a, b); });
+
+    cout << format("Part 2: {}\n", l);
 }
